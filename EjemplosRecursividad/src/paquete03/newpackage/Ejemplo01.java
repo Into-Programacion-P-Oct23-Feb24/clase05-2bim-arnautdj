187522b194e8a012421d28dbffc60c9a15f1045b
arnautdj <sfrosales@utpl.edu.ec>
2024-01-11 15:52:15 -0500

    paquete3

---
+++ EjemplosRecursividad/src/paquete03/newpackage/Ejemplo01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete03.newpackage;
+
+/**
+ *
+ * @author santy
+ */
+public class Ejemplo01 {
+    public static void main(String[] args) {
+        
+    }
+    public static String[][] obtenerCadena() {
+        String [][] cadena = {{"LOJA", "CUENCA", "QUITO"},
+            {"GUAYAQUIL", "ZAMORA", "MACHALA"}};
+        return cadena;
+    }
+}
